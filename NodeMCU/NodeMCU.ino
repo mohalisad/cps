5555fe31b32d7b03696b676fca2cea392900a211
msadraei <msadraei@icloud.com>
2019-06-11 17:27:39 +0430

    node wifi ack init

--- NodeMCU/NodeMCU.ino
+++ NodeMCU/NodeMCU.ino
@@ -1,30 +1,16 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
+#include "wifi.h"
 
 #define WIFI_SSID   "MiWiFi"
 #define WIFI_PASS   "p:159357"
 #define LISTEN_PORT 4210
 #define MAX_LEN     20
 
-const char* ssid = WIFI_SSID;
-const char* password = WIFI_PASS;
-
-WiFiUDP Udp;
-unsigned int localUdpPort = LISTEN_PORT;
 char incomingPacket[MAX_LEN];
-char  replyPacket[] = "Hi there! Got the message :-)";
 
-void init_wifi(){
-  IPAddress staticIP(192, 168, 1, 50);
-  IPAddress gateway(192, 168, 1, 1);
-  IPAddress subnet(255, 255, 255, 0);
-  WiFi.config(staticIP, subnet, gateway);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-  }
-  Udp.begin(localUdpPort);
+byte[2] make_ack(byte[4] incomingPacket){
+  short seqNumber;
+  seqNumber = (incomingPacket[2]&0x1F)*16 + (incomingPacket[3]>>4);
+  return seqNumber;
 }
 
 void setup()
@@ -41,8 +27,8 @@ void loop()
   {
     int len = Udp.read(incomingPacket, MAX_LEN);
     Serial.write(incomingPacket,len);
-    //Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-    //Udp.write(replyPacket);
-    //Udp.endPacket();
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(make_ack(incomingPacket));
+    Udp.endPacket();
   }
 }
---
+++ NodeMCU/wifi.h
@@ -0,0 +1,21 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASS;
+
+WiFiUDP Udp;
+unsigned int localUdpPort = LISTEN_PORT;
+
+void init_wifi(){
+  IPAddress staticIP(192, 168, 1, 50);
+  IPAddress gateway(192, 168, 1, 1);
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.config(staticIP, subnet, gateway);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+  Udp.begin(localUdpPort);
+}
